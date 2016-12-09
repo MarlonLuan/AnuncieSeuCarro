29af5c21aa243518394a7c6600c40deee0930afe
MarlonLuan <marlon-luan+github@hotmail.com>
2016-12-09 09:54:22 -0200

    Criação do Projeto

---
+++ app/src/main/java/com/marlonluan/anuncieseucarro/AnuncieSeuCarroActivity.java
@@ -0,0 +1,13 @@
+package com.marlonluan.anuncieseucarro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnuncieSeuCarroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anuncie_seu_carro);
+    }
+}
