622ff8846aed565aef6c6ff65ca4550c00acf1e7
MarlonLuan <marlon-luan+github@hotmail.com>
2016-12-09 11:57:49 -0200

    Adicionado Botão no Menu Principal
    Adicionado Botão "Meus Anúncios" no Menu Principal para acesso a
    listagem de Hoteis

--- app/src/main/java/com/marlonluan/anuncieseucarro/MenuPrincipalActivity.java
+++ app/src/main/java/com/marlonluan/anuncieseucarro/MenuPrincipalActivity.java
@@ -1,13 +1,34 @@
 package com.marlonluan.anuncieseucarro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.marlonluan.anuncieseucarro.hotel.*;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    Button mBtnMeusAnuncios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        mBtnMeusAnuncios = (Button) findViewById(R.id.btnMeusAnuncios);
+        mBtnMeusAnuncios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartHotelActivity();
+            }
+        });
+    }
+
+
+    public void StartHotelActivity(){
+        Intent it = new Intent(this, HotelActivity.class);
+        startActivity(it);
     }
 }
